d7a98982b0143c4143990d653414f4d91307d48a
Honti Gergely <honti@honti.us>
2014-10-04 21:51:29 +0200

    Àdded AES, Hashes, TDES

---
+++ CSharp_CryptoEngine/CSharp_CryptoEngine/Hashes/SHA256.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Honti.CryptoEngine
+{
+    class SHA256Hash
+    {
+        public static string Encrypt(string plainText)
+        {
+            var sha256 = SHA256.Create();
+            var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+            var str = new StringBuilder();
+            foreach (var t in hash)
+            {
+                str.Append(t.ToString("X1"));
+            }
+            return str.ToString();
+        }
+
+       
+
+    }
+}
---
+++ CSharp_CryptoEngine/CSharp_CryptoEngine/Helpers/EMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Honti.CryptoEngine
+{
+    public enum HashingMethod
+    {
+        MD5,
+        SHA1,
+        SHA256,
+        SHA384,
+        SHA512
+    }
+
+}
